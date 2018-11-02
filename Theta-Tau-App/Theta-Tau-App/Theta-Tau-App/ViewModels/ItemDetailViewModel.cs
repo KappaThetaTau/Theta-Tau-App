003acf531b63d41a40a3d3f7af55fd3d60940303
Rishab Pohane <pohanerishab@gmail.com>
2018-11-01 19:12:20 -0500

    First commit!

---
+++ Theta-Tau-App/Theta-Tau-App/Theta-Tau-App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Theta_Tau_App.Models;
+
+namespace Theta_Tau_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Theta-Tau-App/Theta-Tau-App/Theta-Tau-App/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Theta_Tau_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
