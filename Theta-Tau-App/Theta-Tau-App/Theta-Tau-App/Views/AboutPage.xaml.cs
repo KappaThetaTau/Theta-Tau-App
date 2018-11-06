a99f08b867e7e2c9099111f29533949a23ba4491
Kanchi Shah <khshah6@illinois.edu>
2018-11-05 20:30:59 -0600

    updated about us page

--- Theta-Tau-App/Theta-Tau-App/Theta-Tau-App/Views/AboutPage.xaml.cs
+++ Theta-Tau-App/Theta-Tau-App/Theta-Tau-App/Views/AboutPage.xaml.cs
@@ -10,7 +10,31 @@ namespace Theta_Tau_App.Views
     {
         public AboutPage()
         {
-            InitializeComponent();
+            var title = new Label
+            {
+                Text = "About TT",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Maroon,
+                FontAttributes = FontAttributes.Bold,
+            };
+
+            var description = new Label
+            {
+                Text = "Theta Tau is more than just a co-ed fraternity to us; it is a brotherhood. Here, we have built life-long relationships and learnt to navigate our school and career with the support of each other.",
+                TextColor = Color.Gold,
+                FontAttributes = FontAttributes.Italic,
+            };
+
+
+            Content = new StackLayout
+            {
+                BackgroundColor = Color.Black,
+                Padding = 30,
+                Spacing = 10,
+                Children = { title, description }
+            };
+
         }
     }
 }
